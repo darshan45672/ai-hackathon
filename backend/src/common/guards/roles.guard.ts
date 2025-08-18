48871f213a156e7e9c174fab482cb0a5d6635f09
darshan45672 <drshnbhandary@gmail.com>
2025-08-18 18:44:20 +0530

    converted the apis to graphql

--- backend/src/common/guards/roles.guard.ts
+++ backend/src/common/guards/roles.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { ROLES_KEY } from '../decorators/roles.decorator';
 
 @Injectable()
@@ -14,7 +15,20 @@ export class RolesGuard implements CanActivate {
     if (!requiredRoles) {
       return true;
     }
-    const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.role?.includes(role));
+    
+    // Get user from either HTTP or GraphQL context
+    let user;
+    const request = context.switchToHttp().getRequest();
+    
+    if (request) {
+      user = request.user;
+    } else {
+      // Handle GraphQL context
+      const gqlContext = GqlExecutionContext.create(context);
+      const { req } = gqlContext.getContext();
+      user = req.user;
+    }
+    
+    return requiredRoles.some((role) => user?.role === role);
   }
 }
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersResolver } from './users.resolver';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersResolver],
   exports: [UsersService],
 })
 export class UsersModule {}
